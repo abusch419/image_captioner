a289f4d03e6351b9ba14959d8fa7c6e2e92fa936
alexandereversbusch <abusch419@gmail.com>
2023-11-30 12:22:23 -0500

    Switch models to base.

--- image_captioner.py
+++ image_captioner.py
@@ -4,8 +4,8 @@ from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
 def get_caption(img_path):
-    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
-    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
 
     img = Image.open(img_path)
 
